a0571cb718374a3bb84df7810f79d5bbf9ff0786
tong <luck938938@hotmail.com>
2019-08-02 13:11:38 -0400

    building our own annotation

---
+++ src/com/springdemo/mvc/validation/CourseCodeContraintValidator.java
@@ -0,0 +1,30 @@
+package com.springdemo.mvc.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CourseCodeContraintValidator 
+		implements ConstraintValidator<CourseCode, String>{
+
+	private String coursePrefix;
+	
+	@Override
+	public void initialize(CourseCode theCourseCode) {
+		this.coursePrefix=theCourseCode.value();
+	}
+	
+	@Override
+	public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
+		
+		boolean result;
+		
+		if(theCode!=null) {
+			result=theCode.startsWith(coursePrefix);
+		}else {
+			result=true;
+		}
+		
+		return result;
+	}
+
+}
